31e4524b63a62781886d6d6c9de6a2083f575a16
maxx.dev <90531237+MARKMUSYIMI@users.noreply.github.com>
2023-02-25 11:46:44 +0300

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,18 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS_0.7_ModByPiash.cmd'
+
+$FilePath = "$env:TEMP\IAS.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
